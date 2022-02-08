6c08b481c197412fae639d819eef39a0989050f9
ervanrahadian <ervanrahadian@gmail.com>
2022-02-08 23:41:15 +0700

    integrate pwa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import ApolloProvider from "./ApolloProvider";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(ApolloProvider, document.getElementById("root"));
 
 reportWebVitals();
+serviceWorker.register();
